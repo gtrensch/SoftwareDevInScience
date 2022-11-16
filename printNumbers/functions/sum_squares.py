c8f68f6e576635633cf7a4389efe88a901ac7474
MilenaVeneva <milena.p.veneva@gmail.com>
2022-11-16 12:56:59 +0200

    implemented a function that finds the sum of squares of the first n numbers; implemented a suitable unittest to check that the function works correctly

---
+++ printNumbers/functions/sum_squares.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+#
+# factorial.py
+#
+# This file is part of printNumbers.
+#
+# Copyright (C) 2017 G. Trensch, Simulation & Datalab Neuroscience, JSC, FZ Juelich
+#
+# printNumbers is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 2 of the License, or
+# (at your option) any later version.
+#
+# printNumbers is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with printNumbers.  If not, see <http://www.gnu.org/licenses/>.
+
+import math
+
+def Factorial(n):
+    '''
+    :param n:  Operand
+    :return:   n!
+    '''
+    return(n * (n + 1) * (2 * n + 1) / 6)
---
+++ printNumbers/unittests/test_sum_squares.py
@@ -0,0 +1,59 @@
+# -*- coding: utf-8 -*-
+#
+# test_sum_squares.py
+#
+# This file is part of PrintNumbers.
+#
+# Copyright (C) 2017 G. Trensch, Simulation & Datalab Neuroscience, JSC, FZ Juelich
+#
+# PrintNumbers is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 2 of the License, or
+# (at your option) any later version.
+#
+# PrintNumbers is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with PrintNumbers.  If not, see <http://www.gnu.org/licenses/>.
+
+#
+# Unit tests: 'factorial'.
+#
+
+import sys, os
+sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
+
+import unittest
+from functions.factorial import *
+
+class TestFactorial(unittest.TestCase):
+
+    def test_value_0(self):
+        self.assertEqual(Factorial(0), 0)
+
+    def test_value_1(self):
+        self.assertEqual(Factorial(1), 1)
+
+    def test_value_2(self):
+        self.assertEqual(Factorial(2), 4)
+
+    def test_value_3(self):
+        self.assertEqual(Factorial(3), 14)
+
+    def test_value_10(self):
+        self.assertEqual(Factorial(10), 10 * 11 * 21 / 6)
+
+
+def suite():
+    suite = unittest.makeSuite(TestFactorial, 'test')
+    return suite
+
+def run():
+    runner = unittest.TextTestRunner(verbosity = 2)
+    runner.run(suite())
+
+if __name__ == "__main__":
+    run()
