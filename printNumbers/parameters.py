cb4888b57bcfadd6cae71b8fcac9ad9bb5682a27
schererc <christophscherer83@gmail.com>
2022-11-17 13:17:12 +0100

    implementation of new function gaussian_summation

--- printNumbers/parameters.py
+++ printNumbers/parameters.py
@@ -29,6 +29,7 @@ CONST_DEF_OPERAND_VAL = 10
 CONST_MAX_OPERAND_VAL = 20
 CONST_FUNC_CODE_FIBONACCI = 0
 CONST_FUNC_CODE_FACTORIAL = 1
+CONST_FUNC_CODE_GAUSSIAN_SUMMATION = 2
 
 class Parameters(object):
 
@@ -43,6 +44,8 @@ class Parameters(object):
             self.functionIndex = CONST_FUNC_CODE_FIBONACCI
         elif cmdLineArgs['--factorial']:
             self.functionIndex = CONST_FUNC_CODE_FACTORIAL
+        elif cmdLineArgs['--gaussiansummation']:
+            self.functionIndex = CONST_FUNC_CODE_GAUSSIAN_SUMMATION
 
     @property
     def operand(self):
